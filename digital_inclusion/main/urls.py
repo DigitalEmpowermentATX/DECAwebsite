7cf18a9b2b6fd58c18e87d7aaefa950d9388cce8
janabimustafa <mustafa@mustafataleb.com>
2018-04-01 14:18:32 -0500

    Added basic login page

--- digital_inclusion/main/urls.py
+++ digital_inclusion/main/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls import url
 from main import views
 
 urlpatterns = [
-    url(r'about/', views.about, name="about"),
-    url(r'', views.index, name="index"),
+    url(r'about/$', views.about, name="about"),
+    url(r'$', views.index, name="index"),
 ]
--- digital_inclusion/user_management/views.py
+++ digital_inclusion/user_management/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
+from django.views.decorators.http import require_http_methods
 # Create your views here.
+# @require_http_methods(['GET', 'POST'])
+# def login(request: HttpRequest):
+#     if request.method == 'GET':
+
