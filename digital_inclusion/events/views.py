1050dd48ae420856cce0e2ae0f3650a07c34c24e
janabimustafa <mustafa@mustafataleb.com>
2018-04-12 00:37:00 -0500

    Fixed errors for event creation

--- digital_inclusion/events/views.py
+++ digital_inclusion/events/views.py
@@ -3,21 +3,22 @@ import json
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from events.forms import EventInputForm
-
+from organization_management.models import Organization
 # Create your views here.
+
+
 def add(request):
-    if request.method == "GET":
-        f = EventInputForm()
-        return render(request,
-                      template_name="event_input.jinja2",
-                      context={"form": f})
-    elif request.method == "POST":
+    if request.method == "POST":
         f = EventInputForm(request.POST)
         if f.is_valid():
             f.save()
             return HttpResponse("Success!")
-        else:
-            return HttpResponse(json.dumps([str(x) for x in f.errors]))
+    elif request.method == "GET":
+        f = EventInputForm()
+    return render(request,
+                  template_name="event_input.jinja2",
+                  context={"form": f})
+
 
 def calendar(request):
     return HttpResponse("Events Page")
