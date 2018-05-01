65990cbb3c26c368ce29c2597229f073a06ce3c5
janabimustafa <mustafa@mustafataleb.com>
2018-04-30 23:52:41 -0500

    Added editable content ui components

--- digital_inclusion/main/views.py
+++ digital_inclusion/main/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.conf import settings
 from organization_management.models import Organization, Branch
 from events.models import Event
+from main.models import UIComponent
 
 # Create your views here.
 def index(request: HttpRequest):
@@ -14,11 +15,14 @@ def index(request: HttpRequest):
             "show_header": False,
             "default_view": "listMonth"
         },
-        "featured_orgs": featured_orgs
+        "featured_orgs": featured_orgs,
+        "content": UIComponent.objects.get(name="Index Content"),
+        "resources": UIComponent.objects.get(name="Index Resources"),
+        "highlights": UIComponent.objects.get(name="Index Highlights"),
     })
 
 def about(request: HttpRequest):
-    return render(request, template_name="about.jinja2")
+    return render(request, template_name="about.jinja2", context={"content": UIComponent.objects.get(name="About")})
 
 def map(request: HttpRequest):
     return render(request, template_name="map.jinja2", context={
