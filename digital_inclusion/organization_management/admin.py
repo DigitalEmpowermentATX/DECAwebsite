b8418e5188eccd342adff142d5bdf42f65c1cd80
janabimustafa <mustafa@mustafataleb.com>
2018-05-03 17:02:01 -0500

    Added branch import and export

--- digital_inclusion/organization_management/admin.py
+++ digital_inclusion/organization_management/admin.py
@@ -10,8 +10,13 @@ class OrganizationResource(resources.ModelResource):
         model = models.Organization
 class OrganizationAdmin(ImportExportActionModelAdmin):
     resource_class = OrganizationResource
+class BranchResource(resources.ModelResource):
+    class Meta:
+        model = models.Branch
+class BranchAdmin(ImportExportActionModelAdmin):
+    resource_class = BranchResource
 admin.site.register(models.Organization, OrganizationAdmin)
-admin.site.register(models.Branch)
+admin.site.register(models.Branch, BranchAdmin)
 admin.site.register(models.Language)
 admin.site.register(models.TrainingProgram)
 admin.site.register(models.OpeningHours)
